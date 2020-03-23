c08c9d3fa29fe0bdd1daeb7c789ca6f87fb47e04
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-23 19:47:48 +0600

    Cart Product's keys & Quantities added in local storage

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -4,28 +4,26 @@ import fakeData from '../../fakeData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './SingleProduct.css'
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 
 const SingleProduct = () => {
     const { productKey } = useParams();
     const product = fakeData.find(pd => pd.key === productKey)
 
     const [quantity, setQuantity] = useState(1);
-    // const [price, setPrice] = useState(product.price);
 
-   
+   const hanldeCart = (cartProduct) => {
+        addToDatabaseCart(cartProduct.key, quantity)
+   }
 
     const handlePlusIcon = () => {
         const ProductQuantity = quantity + 1;
-        // const ProductPrice = ProductQuantity * price;
         setQuantity(ProductQuantity);
-        // setPrice(ProductPrice);
     }
     const handleMinusIcon = () => {
         if(quantity >= 2) {
             const ProductQuantity = quantity - 1;
-            // const ProductPrice = ProductQuantity * price;
             setQuantity(ProductQuantity);
-            // setPrice(ProductPrice);
         }
     }
 
@@ -37,14 +35,14 @@ const SingleProduct = () => {
                         <h2>{product.name}</h2>
                         <p>{product.description}</p>
                         <div className="ProductPrice d-flex align-items-center">
-                            <h4>&#36; <span id="price">{quantity > 0 ? product.price * quantity : product.price}</span></h4>
+                            <h4>&#36; <span id="price">{quantity > 0 ? (product.price * quantity).toFixed(2) : product.price}</span></h4>
                             <div className="ProductQuantity d-flex">
                                 <button onClick={handleMinusIcon}>-</button>
                                 <span id="quantity">{ quantity }</span>
                                 <button onClick={handlePlusIcon}>+</button>
                             </div>
                         </div>
-                        <button className="CartBtn"><FontAwesomeIcon icon={faShoppingCart} /><span>Add</span></button>
+                        <button onClick={() => hanldeCart(product)} className="CartBtn"><FontAwesomeIcon icon={faShoppingCart} /><span>Add</span></button>
                     </div>
                     <div className="ProductImage">
                         <img src={product.img} alt=""/>
