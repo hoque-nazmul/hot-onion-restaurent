a24f7de90c9edbfb8492a9c9de4a3695de7fc653
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-06 17:45:52 +0600

    payment success & failure message added

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  CardElement,
-  useStripe,
-  useElements,
+    CardElement,
+    useStripe,
+    useElements,
 } from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+    const [paymentError, setPaymentError] = useState(null);
+    const [paymentData, setPaymentData] = useState(null);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    });
-    console.log("stripe integrated", error, paymentMethod);
-  };
+    const stripe = useStripe();
+    const elements = useElements();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-  );
+    if(paymentError) {
+        setTimeout( ()  => {
+            setPaymentError(null);
+        }, 5000)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement),
+        });
+        console.log(error, paymentMethod);
+        if(error) {
+            setPaymentError(error);
+            setPaymentData(null);
+        }else{
+            setPaymentData(paymentMethod);
+            setPaymentError(null);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>
+                Pay
+            </button>
+
+            {
+                paymentError && <p style={{ color: 'red', marginTop: '15px' }}>{paymentError}</p>
+            }
+            {
+                paymentData && <p style={{ color: 'green', marginTop: '15px' }}>Payment Successfull</p>
+            }
+        </form>
+    );
 };
 
 export default CheckoutForm;
